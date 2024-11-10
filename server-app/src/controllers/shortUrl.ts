5d81a02c9461fba3938a69d8f3b24a6bfac48438
Jayandra Hamal <salonhamal278@gmail.com>
2024-11-10 21:51:42 +0545

    Add Create Short URL Function

--- server-app/src/controllers/shortUrl.ts
+++ server-app/src/controllers/shortUrl.ts
@@ -6,7 +6,19 @@ export const createUrl = async (
   req: express.Request,
   res: express.Response
 ) => {
+  try {
+    const { fullUrl } = req.body
+    const urlFound = await urlModel.find({ fullUrl })
+    if (urlFound.length > 0) {
+      return res.status(409).json({ message: "URL already exists" });
+    } else {
+      const shortUrl = await urlModel.create({ fullUrl });
+      return res.status(201).json(shortUrl);
+    }
 
+  } catch (error) {
+    res.status(500).json({ message: "something went wrong" })
+  }
 }
 
 
@@ -14,19 +26,20 @@ export const getAllUrl = async (
   req: express.Request,
   res: express.Response
 ) => {
-  
+ 
+
 }
 
 export const getUrl = async (
   req: express.Request,
   res: express.Response
 ) => {
-  
+
 }
 
 export const deleteUrl = async (
   req: express.Request,
   res: express.Response
 ) => {
-  
+
 }
