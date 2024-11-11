24ff1d60b254adaa4935ede635c94803736bea2b
Jayandra Hamal <salonhamal278@gmail.com>
2024-11-11 21:37:10 +0545

    render Data Table List

--- client-app/url-shortner/src/components/Container/Container.tsx
+++ client-app/url-shortner/src/components/Container/Container.tsx
@@ -1,12 +1,29 @@
 import * as React from 'react';
 import FormContainer from '../FormContainer/FormContainer';
+import { UrlData } from '../../interface/UrlData';
+import axios from 'axios';
+import { serverUrl } from '../../helpers/Constant';
+import DataTable from '../DataTable/DataTable';
 
 
 
 const Container: React.FunctionComponent = () => {
+  const [data,setData] = React.useState<UrlData[]>([])
+
+  const fetchTableData = async () => {
+    const response = await axios.get(`${serverUrl}/shortUrl`);
+    console.log("The response from server is: " , response)
+    setData(response.data);
+   
+  };
+
+  React.useEffect(() => {
+    fetchTableData();
+  }, [])
   return(
     <>
       <FormContainer />
+      <DataTable data={data}/>
     </>
   ) ;
 };
--- client-app/url-shortner/src/components/DataTable/DataTable.tsx
+++ client-app/url-shortner/src/components/DataTable/DataTable.tsx
@@ -1,7 +1,41 @@
 import * as React from 'react';
+import { UrlData } from '../../interface/UrlData';
+import { Link } from 'react-router-dom';
+import { serverUrl } from '../../helpers/Constant';
+
+interface IDataTableProps {
+  data: UrlData[];
+}
 
 
-const DataTable: React.FunctionComponent = () => {
+const DataTable: React.FunctionComponent<IDataTableProps> = (props) => {
+  const {data} = props;
+  console.log(data)
+  const renderTableData = () => {
+    return data.map((item) => {
+      return (
+        <tr key={item._id} className='border-b text-white bg-gray-600 hover:bg-white hover:text-gray-800'>
+          <td className='px-6 py-3 '>
+            <Link to={item.fullUrl} target="_blank" rel='noreferrer noopener'>
+              {item.fullUrl}
+            </Link>
+          </td>
+
+          <td className='px-6 py-3 '>
+            <Link to={`${serverUrl}/shortUrl/${item.shortUrl}`} target="_blank" rel='noreferrer noopener'>
+              {item.shortUrl}
+            </Link>
+          </td>
+          
+
+          <td className='px-6 py-3 '>{item.clicks}</td>
+
+          <td className='px-6 py-3'></td>
+        </tr>
+      );
+    })
+  }
+
   return (
     <div className="container mx-auto pt-2 pb-10">
       <div className="relative overflow-x-auto shadow-sm sm:rounded-lg">
