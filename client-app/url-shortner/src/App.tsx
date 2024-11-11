95f0fc9f1b4e84ddf62e4350ae65dcae9cf1dbc5
Jayandra Hamal <salonhamal278@gmail.com>
2024-11-11 13:33:49 +0545

    form handling

--- client-app/url-shortner/src/App.tsx
+++ client-app/url-shortner/src/App.tsx
@@ -3,11 +3,13 @@ import './App.css'
 import Header from './components/Header/Header'
 import Container from './components/Container/Container'
 import Footer from './components/Footer/Footer'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
 
   return (
     <>
+    <Toaster />
       <Header />
       <Container />
       <Footer />
--- client-app/url-shortner/src/components/FormContainer/FormContainer.tsx
+++ client-app/url-shortner/src/components/FormContainer/FormContainer.tsx
@@ -3,6 +3,14 @@ import * as React from 'react';
 
 
 const FormContainer: React.FunctionComponent = () => {
+  const [fullUrl, setFullUrl] = React.useState<string>("")
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Add your URL shortening logic here
+    console.log('Shortened URL:', `urlshortner.link/${fullUrl}`);
+  }
+
   return(
     <div className="container mx-auto p-2">
       <div className=" bg-banner bg-cover rounded-xl my-8 bg-center">
@@ -15,7 +23,7 @@ const FormContainer: React.FunctionComponent = () => {
           free tool to Shorten a URL or reduce link. Use our URL shortner to create a shortened & neat link making it easy to use
         </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="flex">
             <div className="relative w-full">
               <div className="absolute inset-y-0 start-0 flex items-center ps-2 pointer-events-none text-slate-800">
@@ -27,6 +35,8 @@ const FormContainer: React.FunctionComponent = () => {
               placeholder='add your link'
               required
               className='block w-full p-4 ps-32 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500'
+              value={fullUrl}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setFullUrl(e.target.value)}
               />
 
               <button type='submit' className='absolute top-0 end-0 p-2.5 ttext-sm font-medium h-full text-white bg-blue-700 rounded-lg border border-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300'> Shorten URL</button>
